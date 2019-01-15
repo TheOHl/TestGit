7cb453f1ca7e3b31f9f25e7965897673b25c36ae
hl_eclipse <hl@icloud.com>
2019-01-15 15:45:15 +0800

    update 2

--- src/main/java/com/theo/git/Apple.java
+++ src/main/java/com/theo/git/Apple.java
@@ -2,10 +2,7 @@ package com.theo.git;
 
 public class Apple {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("222");
-=======
 		System.out.println("111");
->>>>>>> branch 'master' of https://github.com/theohl/TestGit.git
 	}
 }
